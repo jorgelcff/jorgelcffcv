82ce673ee2c73509ef7b975f73d66f5b21505d9c
Jorge Freitas <80436467+jorgelcff@users.noreply.github.com>
2024-09-11 12:32:28 -0300

    init commit

---
+++ translate.js
@@ -0,0 +1,98 @@
+document.getElementById('en-btn').addEventListener('click', () => {
+  // Tradução para Inglês
+  document.getElementById('name').textContent = 'Jorge Freitas';
+  document.getElementById('title').textContent = 'Software Developer';
+  document.getElementById('phoneLabel').textContent = 'Phone:';
+  document.getElementById('emailLabel').textContent = 'Email:';
+  document.getElementById('github').textContent = 'GitHub';
+  document.getElementById('linkedin').textContent = 'LinkedIn';
+  
+  // Experiência
+  document.getElementById('experienceTitle').textContent = 'Experience';
+  document.getElementById('aconosTitle').textContent = 'Developer - Aconos Tecnologia';
+  document.getElementById('aconosDesc').textContent = 'Front-End Development in Web and Mobile platforms using Angular and Ionic.';
+  document.getElementById('keycoreTitle').textContent = 'CTO - KeyCore IoT and Software House';
+  document.getElementById('keycoreDesc').textContent = 'Design and creation of cross-platform systems and automations using NextJS, Nest, Flutter, and Ionic.';
+  document.getElementById('minsaitTitle').textContent = 'Systems Analyst PL - Minsait Brasil';
+  document.getElementById('minsaitDesc').textContent = 'Analysis and development of projects using AngularJS, React-Native, Flutter, and API Rest.';
+  document.getElementById('aconosTitle2').textContent = 'Developer - Aconos Tecnologia';
+  document.getElementById('aconosDesc2').textContent = 'Front-End Development in Web and Mobile platforms using Angular and Ionic.';
+  document.getElementById('ufpeTitle').textContent = 'Software Engineering Intern - UFPE';
+  document.getElementById('ufpeDesc').textContent = 'Work in the Systems Management of the UFPE Informatics Center.';
+  document.getElementById('crdTitle').textContent = 'Developer - CRD DATA';
+  document.getElementById('crdDesc').textContent = 'Creation of Platform and Application using ReactJS, React-Native, SQL, Javascript, and Typescript.';
+  document.getElementById('aeolusTitle').textContent = 'Web Developer - Aeolus Cloud';
+  document.getElementById('aeolusDesc').textContent = 'Work in back-end and front-end with technologies ReactJs, Next, Prisma, Chakra-ui, and Styled-Components.';
+  document.getElementById('incogniaTitle').textContent = 'PET-FACEPE Scholar - Incognia';
+  document.getElementById('incogniaDesc').textContent = 'Study focused on data analysis and structuring of fraud prevention models.';
+  
+  // Educação
+  document.getElementById('educationTitle').textContent = 'Education';
+  document.getElementById('ufpeEdu').textContent = 'Information Systems, Federal University of Pernambuco (2022 - Present)';
+  document.getElementById('portoEdu').textContent = 'Technical Integrated Course in Systems Development, Porto Digital State Technical School (2019 - 2021)';
+  
+  // Habilidades
+  document.getElementById('skillsTitle').textContent = 'Skills';
+  
+  // Idiomas
+  document.getElementById('languagesTitle').textContent = 'Languages';
+  document.getElementById('portugueseLang').textContent = 'Portuguese (Native)';
+  document.getElementById('englishLang').textContent = 'English (B2)';
+  
+  // Interesses
+  document.getElementById('interestsTitle').textContent = 'Interests';
+  document.getElementById('interest1').textContent = 'Web and Mobile Systems Development';
+  document.getElementById('interest2').textContent = 'IoT Systems Development';
+  document.getElementById('interest3').textContent = 'Process Automation';
+  document.getElementById('interest4').textContent = 'Data Science';
+});
+
+document.getElementById('pt-btn').addEventListener('click', () => {
+  // Tradução para Português
+  document.getElementById('name').textContent = 'Jorge Freitas';
+  document.getElementById('title').textContent = 'Desenvolvedor de Sistemas';
+  document.getElementById('phoneLabel').textContent = 'Telefone:';
+  document.getElementById('emailLabel').textContent = 'Email:';
+  document.getElementById('github').textContent = 'GitHub';
+  document.getElementById('linkedin').textContent = 'LinkedIn';
+  
+  // Experiência
+  document.getElementById('experienceTitle').textContent = 'Experiência';
+  document.getElementById('aconosTitle').textContent = 'Desenvolvedor - Aconos Tecnologia';
+  document.getElementById('aconosDesc').textContent = 'Desenvolvimento Front-End em plataforma Web e Mobile utilizando Angular e Ionic.';
+  document.getElementById('keycoreTitle').textContent = 'CTO - KeyCore IoT and Software House';
+  document.getElementById('keycoreDesc').textContent = 'Concepção e criação de sistemas multiplataformas e automações com NextJS, Nest, Flutter e Ionic.';
+  document.getElementById('minsaitTitle').textContent = 'Analista de Sistemas PL - Minsait Brasil';
+  document.getElementById('minsaitDesc').textContent = 'Análise e desenvolvimento de projetos usando AngularJS, React-Native, Flutter e API Rest.';
+  document.getElementById('aconosTitle2').textContent = 'Desenvolvedor - Aconos Tecnologia';
+  document.getElementById('aconosDesc2').textContent = 'Desenvolvimento Front-End em plataforma Web e Mobile utilizando Angular e Ionic.';
+  document.getElementById('ufpeTitle').textContent = 'Bolsista de Engenharia de Software - UFPE';
+  document.getElementById('ufpeDesc').textContent = 'Atuação na Gerência de Sistemas do Centro de Informática da UFPE.';
+  document.getElementById('crdTitle').textContent = 'Desenvolvedor - CRD DATA';
+  document.getElementById('crdDesc').textContent = 'Criação de Plataforma e Aplicativo usando ReactJS, React-Native, SQL, Javascript e Typescript.';
+  document.getElementById('aeolusTitle').textContent = 'Desenvolvedor Web - Aeolus Cloud';
+  document.getElementById('aeolusDesc').textContent = 'Atuação em back-end e front-end com as tecnologias ReactJs, Next, Prisma, Chakra-ui e Styled-Components.';
+  document.getElementById('incogniaTitle').textContent = 'Bolsista PET-FACEPE - Incognia';
+  document.getElementById('incogniaDesc').textContent = 'Estudo focado na análise de dados e estruturação de modelos de prevenção de fraudes.';
+  
+  // Educação
+  document.getElementById('educationTitle').textContent = 'Formação Acadêmica';
+  document.getElementById('ufpeEdu').textContent = 'Sistemas de Informação, Universidade Federal de Pernambuco (2022 - Atual)';
+  document.getElementById('portoEdu').textContent = 'Curso Técnico Integrado em Desenvolvimento de Sistemas, Escola Técnica Estadual Porto Digital (2019 - 2021)';
+  
+  // Habilidades
+  document.getElementById('skillsTitle').textContent = 'Habilidades';
+  
+  // Idiomas
+  document.getElementById('languagesTitle').textContent = 'Idiomas';
+  document.getElementById('portugueseLang').textContent = 'Português (Nativo)';
+  document.getElementById('englishLang').textContent = 'Inglês (B2)';
+  
+  // Interesses
+  document.getElementById('interestsTitle').textContent = 'Interesses';
+  document.getElementById('interest1').textContent = 'Desenvolvimento de Sistemas Web e Mobile';
+  document.getElementById('interest2').textContent = 'Desenvolvimento de Sistemas IoT';
+  document.getElementById('interest3').textContent = 'Automação de Processos';
+  document.getElementById('interest4').textContent = 'Ciência de Dados';
+
+});
